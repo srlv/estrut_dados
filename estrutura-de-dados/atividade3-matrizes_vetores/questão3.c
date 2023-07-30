5248bd658b91ae595a53639e480ca8d65c691fe5
Servolo P <138056513+srlv@users.noreply.github.com>
2023-07-30 10:15:44 -0300

    resolução

--- estrutura-de-dados/atividade3-matrizes_vetores/questão3.c
+++ estrutura-de-dados/atividade3-matrizes_vetores/questão3.c
@@ -9,20 +9,27 @@ aleatórios entre 1 e 100, e em seguida, exiba a matriz resultante.*/
 #include <stdlib.h>
 #include <time.h>
 #include <locale.h>
+
 int main(void) {
     int n, i, c;
-    setlocale(LC_ALL,"portuguese");
+
+    setlocale(LC_ALL, "portuguese");
+
+    // Solicita ao usuário o número de linhas e colunas para a matriz quadrada
     printf("Informe o número da matriz quadrada: ");
     scanf("%d", &n);
+
+    // Alocação dinâmica de memória para a matriz quadrada de inteiros
     int **matriz = (int**)malloc(n * sizeof(int*));
     if (matriz == NULL) {
         printf("Erro de alocação de memória\n");
         exit(1);
+    } else {
+        printf("Memória alocada com sucesso\n");
     }
-    else{
-    	printf("memória alocada com sucesso\n");
-	}
-    srand(time(NULL));
+
+    // Gera valores aleatórios para preencher a matriz usando a função rand()
+    srand(time(NULL)); // Inicializa o gerador de números aleatórios com o tempo atual
     for (i = 0; i < n; i++) {
         matriz[i] = (int*)malloc(n * sizeof(int));
         if (matriz[i] == NULL) {
@@ -30,19 +37,26 @@ int main(void) {
             return 1;
         }
         for (c = 0; c < n; c++) {
-            matriz[i][c] = rand() % 100 + 1;
+            matriz[i][c] = rand() % 100 + 1; // Gera um valor entre 1 e 100
         }
     }
-    printf("sua matriz é\n");
+
+    // Imprime a matriz gerada
+    printf("Sua matriz é:\n");
     for (i = 0; i < n; i++) {
         for (c = 0; c < n; c++) {
             printf(" %d ", matriz[i][c]);
         }
         printf("\n");
     }
+
+    // Libera a memória alocada para cada linha da matriz
     for (i = 0; i < n; i++) {
-        free(matriz);
+        free(matriz[i]);
     }
+
+    // Libera a memória alocada para a matriz (vetor de ponteiros)
     free(matriz);
+
     return 0;
 }
