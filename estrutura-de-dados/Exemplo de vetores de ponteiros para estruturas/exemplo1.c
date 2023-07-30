cfadd36ca26aa91cf88cf678eff1b4e1ecb8854b
Servolo P <138056513+srlv@users.noreply.github.com>
2023-07-30 10:31:07 -0300

    exemplo sala de aula dia 18/07/2023

--- estrutura-de-dados/Exemplo de vetores de ponteiros para estruturas/exemplo1.c
+++ estrutura-de-dados/Exemplo de vetores de ponteiros para estruturas/exemplo1.c
@@ -1,87 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct aluno{
-	
-	int mat;
-	char nome[81];
-	char end[121];
-	char tel[21];
-	
+// Definição da estrutura "aluno" com os campos matrícula, nome, endereço e telefone.
+struct aluno {
+    int mat;
+    char nome[81];
+    char end[121];
+    char tel[21];
 };
 
 typedef struct aluno Aluno;
 
 #define MAX 100
-Aluno* tab[MAX];
 
+Aluno* tab[MAX]; // Vetor de ponteiros de alunos.
 
-void inicializar (int n,Aluno** tab){
-	
-	int i;
-	for(i=0;i<n;i++)
-	tab[i]=NULL;
+// Função para inicializar o vetor de ponteiros de alunos, atribuindo NULL a cada posição.
+void inicializar(int n, Aluno** tab) {
+    for (int i = 0; i < n; i++)
+        tab[i] = NULL;
 }
 
-void preencher (int n, Aluno** tab, int i){
-	if(i<0 || i>=n){
-	printf("indice fora do limite do vetor\n");
-	exit(1);
-	}
-	if(tab[i]==NULL){
-		tab[i]=(Aluno*)malloc(sizeof(Aluno));
-	}
-	
-	printf("entre com a matricula \n");
-	scanf("%d",&tab[i]->mat);
-	printf("entre com o nome: \n");
-	scanf(" %80[^\n]",tab[i]->nome);
-	printf("entre com o endereco: \n");
-	scanf(" %180[^\n]",tab[i]->end);
-	printf("entre com o telefone: \n");
-	scanf(" %20[^\n]",tab[i]->tel);
+// Função para preencher os dados de um aluno em uma posição do vetor.
+void preencher(int n, Aluno** tab, int i) {
+    if (i < 0 || i >= n) {
+        printf("Índice fora do limite do vetor.\n");
+        exit(1);
+    }
+    if (tab[i] == NULL) {
+        tab[i] = (Aluno*)malloc(sizeof(Aluno)); // Aloca espaço para o aluno na memória.
+    }
+
+    printf("Entre com a matrícula: ");
+    scanf("%d", &tab[i]->mat);
+
+    printf("Entre com o nome: ");
+    scanf(" %80[^\n]", tab[i]->nome);
+
+    printf("Entre com o endereço: ");
+    scanf(" %120[^\n]", tab[i]->end);
+
+    printf("Entre com o telefone: ");
+    scanf(" %20[^\n]", tab[i]->tel);
 }
 
-void retira(int n,Aluno** tab, int i){
-	if(i<0||i>=n){
-		printf("indice fora do limite do vetor \n");
-		exit(1);
-	}
-	if(tab[i]!=NULL){
-		free(tab[i]);
-		tab[i]=NULL;
-	}	
+// Função para retirar um aluno de uma posição do vetor e liberar a memória alocada para ele.
+void retira(int n, Aluno** tab, int i) {
+    if (i < 0 || i >= n) {
+        printf("Índice fora do limite do vetor.\n");
+        exit(1);
+    }
+    if (tab[i] != NULL) {
+        free(tab[i]); // Libera o espaço de memória alocado para o aluno.
+        tab[i] = NULL;
+    }
 }
 
-void imprimir (int n,Aluno** tab,int i){
-	if(i<0||i>=n){
-		printf("indice fora do limite do veto\n");
-		exit(1);
-	}
-	if (tab[i]!=NULL){
-		printf("matricula:%d\n",tab[i]->mat);
-		printf("nome: %s\n",tab[i]->nome);
-		printf("endereco: %s\n",tab[i]->end);
-		printf("telefone: %s\n",tab[i]->tel);
-	}
+// Função para imprimir os dados de um aluno de uma posição do vetor.
+void imprimir(int n, Aluno** tab, int i) {
+    if (i < 0 || i >= n) {
+        printf("Índice fora do limite do vetor.\n");
+        exit(1);
+    }
+    if (tab[i] != NULL) {
+        printf("Matrícula: %d\n", tab[i]->mat);
+        printf("Nome: %s\n", tab[i]->nome);
+        printf("Endereço: %s\n", tab[i]->end);
+        printf("Telefone: %s\n", tab[i]->tel);
+    }
 }
 
-void imprimi_tudo(int n, Aluno** tab){
-	int i;for(i=0;i<n;i++){
-		imprimir(n,tab,i);
-	}
+// Função para imprimir todos os alunos do vetor.
+void imprimi_tudo(int n, Aluno** tab) {
+    for (int i = 0; i < n; i++) {
+        imprimir(n, tab, i);
+    }
 }
-int main(void){
-	
-	Aluno* tab[10];
-	inicializar(10,tab);
-	preencher(10,tab,0);
-	preencher(10,tab,1);
-	preencher(10,tab,2);
-	imprimi_tudo(10,tab);
-	retira(10,tab,0);
-	retira(10,tab,1);
-	retira(10,tab,2);
-	
-	return 0;
+
+int main(void) {
+    Aluno* tab[10];
+    inicializar(10, tab);
+
+    preencher(10, tab, 0);
+    preencher(10, tab, 1);
+    preencher(10, tab, 2);
+
+    imprimi_tudo(10, tab);
+
+    retira(10, tab, 0);
+    retira(10, tab, 1);
+    retira(10, tab, 2);
+
+    return 0;
 }
