2f6cc38f95528b06aa4490e5f3e8be198457a9b5
Servolo P <138056513+srlv@users.noreply.github.com>
2023-07-30 10:21:00 -0300

    resolução

--- estrutura-de-dados/atividade de union e enum/questão1.c
+++ estrutura-de-dados/atividade de union e enum/questão1.c
@@ -8,41 +8,54 @@ deve ler os dados de uma pessoa e imprimir os dados da pessoa.*/
 #include<stdlib.h>
 #include<locale.h>
 
-enum boolean{
-	masculino,
-	feminino
+// Definição do enum "boolean" com as constantes "masculino" e "feminino"
+enum boolean {
+    masculino,
+    feminino
 };
+
+// Criação do typedef "bo" para o enum "boolean"
 typedef enum boolean bo;
 
-typedef struct Pessoa{
-	char nome[20];
+// Definição da estrutura "Pessoa" com campos para o nome, idade e gênero
+typedef struct Pessoa {
+    char nome[20];
     int idade;
     bo genero;
-}pessoa;
- 
-int main(void){
-	setlocale (LC_ALL,"portuguese");
-    pessoa dados;
-	printf("informe seu nome\n");
-	scanf(" %s", dados.nome);
-	
-	printf("informe sua idade\n");
-	scanf("%d", &dados.idade);
+} pessoa;
 
-	bo val;
-	printf("informe seu sexo: 0 para masculino ou 1 para feminino\n");
-	scanf("%d", &val);
-	
-	if(val==0){
-		printf("masculino\n");
-	}
-	if(val==1){
-		printf("feminino\n");
-	}
-    
-    printf("seu nome é: %s\n",dados.nome);
-    printf("voce tem: %d anos\n",dados.idade); 
-    printf("e seu sexo escolhido foi o número: %d\n",dados.genero);
-    
-	return 0;
-}
+int main(void) {
+    setlocale(LC_ALL, "portuguese");
+    pessoa dados;
+
+    // Coleta do nome da pessoa
+    printf("Informe seu nome: ");
+    scanf(" %s", dados.nome);
+
+    // Coleta da idade da pessoa
+    printf("Informe sua idade: ");
+    scanf("%d", &dados.idade);
+
+    // Coleta do gênero da pessoa (0 para masculino ou 1 para feminino)
+    int val;
+    printf("Informe seu sexo: 0 para masculino ou 1 para feminino: ");
+    scanf("%d", &val);
+
+    // Atribui o valor do gênero informado à variável "genero" da estrutura "Pessoa"
+    dados.genero = (val == 0) ? masculino : feminino;
+
+    // Imprime o gênero da pessoa com base na escolha feita
+    if (val == 0) {
+        printf("Masculino\n");
+    }
+    if (val == 1) {
+        printf("Feminino\n");
+    }
+
+    // Imprime o nome, idade e gênero da pessoa
+    printf("Seu nome é: %s\n", dados.nome);
+    printf("Você tem: %d anos\n", dados.idade);
+    printf("E seu sexo escolhido foi o número: %d\n", dados.genero);
+
+    return 0;
+}	
