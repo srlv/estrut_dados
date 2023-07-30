09c4a1dfb617069e50dd427c699d42acbfdbfb40
Servolo P <138056513+srlv@users.noreply.github.com>
2023-07-30 09:55:27 -0300

    Update questão4.c

--- estrutura-de-dados/atividades/ponteiros/questão4.c
+++ estrutura-de-dados/atividades/ponteiros/questão4.c
@@ -5,17 +5,22 @@ void calcula_hexagono(float l, float *area, float *perimetro); A área e o perí
 hexágono regular são dados, respectivamente, por:
 */
 
+//resolução:
+
 #include <stdio.h>
 #include <math.h>
 #include<locale.h>
 #include<stdlib.h>
 
-void calcula_hexagono(float l, float *area, float *perimetro){
-    *perimetro = 6*l;
+void calcula_hexagono(float l, float *area, float *perimetro){ /*função para criar calculo do hexagono*/
+    *perimetro = 6*l;             
     *area = (3*pow(l,2)*sqrt(3))/2;  
 }
 
 int main(void){
+
+   //pedindo os dados para o usuário e encontrando o resultado da operação.
+   
     setlocale(LC_ALL,"portuguese");
     float ld, area, per;
     printf("informe o lado ");
