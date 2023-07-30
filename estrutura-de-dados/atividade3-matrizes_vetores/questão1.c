df614a65bfd7359af1598525c23a4abed13d7a89
Servolo P <138056513+srlv@users.noreply.github.com>
2023-07-30 10:10:04 -0300

    questão1.c

--- estrutura-de-dados/atividade3-matrizes_vetores/questão1.c
+++ estrutura-de-dados/atividade3-matrizes_vetores/questão1.c
@@ -7,26 +7,41 @@ elementos do vetor e, por fim, imprima os elementos na ordem inversa.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-int main(){
-	setlocale(LC_ALL,"portuguese");
+
+int main() {
+    setlocale(LC_ALL, "portuguese");
+
     int tam, c;
+
+    // Solicita ao usuário o tamanho do vetor
     printf("Informe o tamanho do vetor: ");
     scanf("%d", &tam);
+
+    // Alocação dinâmica de memória para o vetor com o tamanho informado pelo usuário
     int *vetor = (int*)malloc(tam * sizeof(int));
+	
+    //verifica se a alocação foi feita corretamente
     if (vetor == NULL) {
         printf("Erro na alocação de memória.\n");
         exit(1);
     }
+
+    // Coleta os elementos do vetor digitados pelo usuário
     printf("Informe os elementos do vetor:\n");
     for (c = 0; c < tam; c++) {
         printf("Elemento %d: ", c + 1);
         scanf("%d", &vetor[c]);
     }
+
+    // Imprime os elementos do vetor em ordem inversa
     printf("Elementos do vetor em ordem inversa:\n");
-    for (c= tam - 1; c>= 0; c--) {
+    for (c = tam - 1; c >= 0; c--) {
         printf("%d ", vetor[c]);
     }
     printf("\n");
+
+    // Libera a memória alocada dinamicamente
     free(vetor);
+
     return 0;
 }
