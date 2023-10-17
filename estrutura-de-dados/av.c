c9c38d117250ddc21f15815a66565c8fc4a8856e
Servolo P. <138056513+srlv@users.noreply.github.com>
2023-10-17 19:45:58 +0000

    -

--- estrutura-de-dados/av.c
+++ estrutura-de-dados/av.c
@@ -10,32 +10,35 @@ typedef struct dados_p{
 
 void preenche (dados* rgt){
 	struct dados;
-	printf("informe o seu nome");
-	scanf(" %[^\n]",&rgt->nome);
+	printf("informe o seu nome\n");
+	scanf(" %[^\n]",rgt->nome);
 	
 	printf("informe a disciplina\n");
-	scanf(" %[^\n]", &rgt->disciplina);
+	scanf(" %[^\n]", rgt->disciplina);
 	
 	printf("informe o regime\n");
-	scanf("%d",&rgt->regime_t);
+	scanf("%f",&rgt->regime_t);
 }
 void imprime(dados*rgt){
 	
 	
 	printf("o seu nome é %s\n",rgt->nome);
 	printf("sua disciplina é %s\n", rgt->disciplina);
-	printf("o seu regime de trabalho é %d", rgt->regime_t);
+	printf("o seu regime de trabalho são %.f horas", rgt->regime_t);
 }
 
-void alteraregime(dados){
+void alteraregime(dados* novoslr, float nslr){
 	
 }
 
 
 int main(void){
 	setlocale(LC_ALL,"portuguese");
-	imprime;
-	preenche;
+	dados pessoa;
+
+	preenche(&pessoa);
+	imprime(&pessoa);
+	
 	return 0;
 	
 }
